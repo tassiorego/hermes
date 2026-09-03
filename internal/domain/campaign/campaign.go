3d67a7ea076706067c471e56e140bd9073426ef2
Tássio Rego <tassiorego@gmail.com>
2026-09-02 22:56:48 -0300

    chore: add campaign domain model

---
+++ internal/domain/campaign/campaign.go
@@ -0,0 +1,33 @@
+package campaign
+
+import "time"
+
+type Contact struct {
+	Email string `json:"email"`
+}
+
+type Campaign struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"createdAt"`
+	Content   string    `json:"content"`
+	Contacts  []Contact `json:"contacts"`
+}
+
+func toContacts(emails []string) []Contact {
+	contacts := make([]Contact, len(emails))
+	for i, email := range emails {
+		contacts[i] = Contact{Email: email}
+	}
+	return contacts
+}
+
+func New(name, content string, contacts []string) *Campaign {
+	return &Campaign{
+		ID:        "1",
+		Name:      name,
+		CreatedAt: time.Now(),
+		Content:   content,
+		Contacts:  toContacts(contacts),
+	}
+}
