587a75a9261edad6183a416ac42be56756bdd1e2
Tássio Rego <tassiorego@gmail.com>
2026-09-21 11:21:17 -0300

    chore: setup basic API with Chi router and middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,11 +4,31 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
 )
 
+type Product struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+}
+
 func main() {
 	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+
+	// router.Use(func(next http.Handler) http.Handler {
+	// 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+	// 		var responseTime int64
+
+	// 		start := time.Now()
+	// 		next.ServeHTTP(res, req)
+	// 		responseTime = time.Since(start).Milliseconds()
+	// 		log.Println(req.Method, req.URL.Path, responseTime, "ms")
+	// 	})
+	// })
 	router.Get("/", func(res http.ResponseWriter, req *http.Request) {
 		name := req.URL.Query().Get("name")
 		res.Write([]byte("Hello, " + name + "!"))
@@ -23,6 +43,13 @@ func main() {
 		users := []string{"Alice", "Bob", "Charlie"}
 		response := map[string][]string{"users": users}
 		render.JSON(res, req, response)
+	})
+
+	router.Post("/products", func(res http.ResponseWriter, req *http.Request) {
+		var product Product
+		render.DecodeJSON(req.Body, &product)
+		product.ID = 1
+		render.JSON(res, req, product)
 	})
 
 	http.ListenAndServe(":3000", router)
